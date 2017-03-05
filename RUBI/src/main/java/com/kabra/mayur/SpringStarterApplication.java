74270bc228b3c5f800b7537876fcd66d1fec9770
mayurkabra <mayur.r.kabra@gmail.com>
2017-03-05 09:53:47 -0500

    Skeleton continues

--- RUBI/src/main/java/com/kabra/mayur/SpringStarterApplication.java
+++ RUBI/src/main/java/com/kabra/mayur/SpringStarterApplication.java
@@ -2,7 +2,6 @@ package com.kabra.mayur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
